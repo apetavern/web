ad4d4dafc06d366bcc550ca164aac8d2c2d56dcd
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-01-01 17:42:46 -0400

    Add index route, setup main layout and partials, use proper handlebars package

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+// Dependencies
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('express-handlebars');
 const path = require('path')
+
+// Routes
+const indexRouter = require('./routes/index');
 
 const app = express();
 
+// Handlebars
+const handlebars = hbs.create({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials'
+});
+
+app.engine('hbs', handlebars.engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('main.hbs');
-})
+app.use('/', indexRouter);
 
 const server = app.listen(3000, () => {
     console.log('Application started on port 3000')
