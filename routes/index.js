fce1e2545eabfd7bc8ec938de914fd432698ff7a
Alex <alexnoir03@gmail.com>
2022-01-04 13:06:00 +0000

    Transparent navbar on index page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('index', {
+        isIndex: true
+    });
 });
 
 module.exports = router;
