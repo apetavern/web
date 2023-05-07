d8d8e905da5c2588351b42b7cca7999da30a3cf9
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-05-07 20:40:01 -0300

    Set launch.json for VS Code users

---
+++ src/components/Header.svelte
@@ -0,0 +1,8 @@
+<header class="absolute z-50 top-0 left-0 right-0 bg-transparent">
+	<div class="container mx-auto px-4">
+		<div class="relative flex items-center justify-between">
+			<p>ItemA</p>
+			<p>ItemB</p>
+		</div>
+	</div>
+</header>
